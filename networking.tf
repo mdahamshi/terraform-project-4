af45cc1bf66ac9539d862a0dbf8db852837906fa
Mohammad Dahamshi <mohammad.dahamshi@gmail.com>
2026-08-03 17:06:31 +0000

    test SG

--- networking.tf
+++ networking.tf
@@ -1,3 +1,7 @@
+##############################
+# VPC & Subnets
+##############################
+
 data "aws_vpc" "default" {
   default = true
 }
@@ -18,11 +22,25 @@ resource "aws_subnet" "private1" {
   vpc_id     = aws_vpc.custom.id
   cidr_block = "10.0.0.0/24"
   tags = {
-    Name   = "subnet-custom-vpc"
+    Name   = "subnet-custom-vpc-1"
     Access = "Private"
   }
 }
-
+resource "aws_subnet" "private2" {
+  vpc_id     = aws_vpc.custom.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name   = "subnet-custom-vpc-2"
+    Access = "Private"
+  }
+}
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.custom.id
+  cidr_block = "10.0.2.0/24"
+  tags = {
+    Name = "subnet-custom-vpc-3"
+  }
+}
 
 # For documintatioin not acvtivly used
 resource "aws_subnet" "not_allowed" {
@@ -31,4 +49,47 @@ resource "aws_subnet" "not_allowed" {
   tags = {
     Name = "subnet-default-vpc"
   }
+}
+##############################
+# Security Groups
+##############################
+
+# 1. Source security group - From where traffic is allowed
+# 2. Compliant security group
+#   2.1 Security group rule
+# 3. Non-compliant security group
+#   3.1 Security group rule
+
+resource "aws_security_group" "source" {
+  name        = "source-sg"
+  description = "SG from where connections are allowed into the DB"
+  vpc_id      = aws_vpc.custom.id
+}
+
+resource "aws_security_group" "compliant" {
+  name        = "compliant-sg"
+  description = "compliant SG"
+  vpc_id      = aws_vpc.custom.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "db" {
+  security_group_id            = aws_security_group.compliant.id
+  referenced_security_group_id = aws_security_group.source.id
+  from_port                    = 5432
+  to_port                      = 5432
+  ip_protocol                  = "tcp"
+}
+
+resource "aws_security_group" "non_compliant" {
+  name        = "non-compliant"
+  description = "nin compliant"
+  vpc_id      = aws_vpc.custom.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "https" {
+  security_group_id = aws_security_group.non_compliant.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
 }
--- rds.tf
+++ rds.tf
@@ -2,13 +2,19 @@
 module "database" {
   source = "./modules/rds"
 
-  project_name       = "project-04-rds-module"
-  security_group_ids = []
+  project_name = "project-04-rds-module"
+  security_group_ids = [
+    aws_security_group.compliant.id
+  ]
   subnet_ids = [
     aws_subnet.private1.id,
+    aws_subnet.private2.id,
   ]
   credentials = {
     username = "db-admin"
     password = "12A3a332"
   }
 }
+
+
+#terraform apply -target="aws_security_group.complaint" -target="aws_vpc_security_group_ingress_rule.db"
