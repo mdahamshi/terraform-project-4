a4a667d5e41b36a35f37e61588dfe0caa47148cb
Mohammad Dahamshi <mohammad.dahamshi@gmail.com>
2026-08-03 16:31:50 +0000

    test file

--- rds.tf
+++ rds.tf
@@ -1,21 +1,14 @@
-resource "aws_db_instance" "default" {
-  allocated_storage    = 10
-  db_name              = "mydb"
-  engine               = "mysql"
-  engine_version       = "8.0"
-  instance_class       = "db.t3.micro"
-  username             = "foo"
-  password             = "foobarbaz"
-  parameter_group_name = "default.mysql8.0"
-  skip_final_snapshot  = true
-}
 
 module "database" {
   source = "./modules/rds"
 
-  project_name = "project-04-rds-module"
+  project_name       = "project-04-rds-module"
+  security_group_ids = []
+  subnet_ids = [
+    aws_subnet.private1.id,
+  ]
   credentials = {
     username = "db-admin"
-    password = "12A3a3"
+    password = "12A3a332"
   }
 }
